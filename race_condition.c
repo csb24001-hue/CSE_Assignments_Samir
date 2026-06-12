3a563a105c74d99bbd7ffd1da585a68f924bc76e
csb24001-hue <csb24001@tezu.ac.in>
2026-06-12 17:14:21 +0530

    assignment 15

---
+++ race_condition.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define THREADS 4
+#define ITERATIONS 100000
+
+int counter = 0;
+
+pthread_mutex_t mutex;
+
+void* increment(void* arg) {
+
+    for(int i = 0; i < ITERATIONS; i++) {
+
+        pthread_mutex_lock(&mutex);
+
+        counter++;
+
+        pthread_mutex_unlock(&mutex);
+    }
+
+    return NULL;
+}
+
+int main() {
+
+    pthread_t threads[THREADS];
+
+    pthread_mutex_init(&mutex, NULL);
+
+    for(int i = 0; i < THREADS; i++) {
+        pthread_create(
+            &threads[i],
+            NULL,
+            increment,
+            NULL
+        );
+    }
+
+    for(int i = 0; i < THREADS; i++) {
+        pthread_join(
+            threads[i],
+            NULL
+        );
+    }
+
+    printf("Final Counter = %d\n",
+           counter);
+
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
